e979075af79c1ea6cc71be33f40f0a0c58319829
Maksym Rozumiei <maksimrozum174@gmail.com>
2025-06-11 05:49:13 +0300

    Add materials crud for teacher users

--- materials/urls.py
+++ materials/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'materials'
 urlpatterns = [
     path('', views.materials_home, name='materials_home'),
+    path('add/', views.CreateMaterialView.as_view(), name='material_add'),
+    path('<int:pk>/edit/', views.UpdateMaterialView.as_view(), name='material_edit'),
+    path('<int:pk>/delete/', views.DeleteMaterialView.as_view(), name='material_delete'),
 ]
--- materials/views.py
+++ materials/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Material, Category
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView
+from users.decorators import role_required
+from django.utils.decorators import method_decorator
 
 
 @login_required(login_url='users:login')
@@ -16,3 +20,26 @@ def materials_home(request):
         'selected_category': int(category_id) if category_id else None,
     }
     return render(request, 'materials/materials_home.html', context)
+
+
+@method_decorator(role_required(['teacher']), name='dispatch')
+class CreateMaterialView(CreateView):
+    model = Material
+    fields = ['title', 'category', 'file']
+    template_name = 'materials/material_form.html'
+    success_url = reverse_lazy('materials:materials_home')
+
+
+@method_decorator(role_required(['teacher']), name='dispatch')
+class UpdateMaterialView(UpdateView):
+    model = Material
+    fields = ['title', 'category', 'file']
+    template_name = 'materials/material_form.html'
+    success_url = reverse_lazy('materials:materials_home')
+
+
+@method_decorator(role_required(['teacher']), name='dispatch')
+class DeleteMaterialView(DeleteView):
+    model = Material
+    template_name = 'materials/material_confirm_delete.html'
+    success_url = reverse_lazy('materials:materials_home')
